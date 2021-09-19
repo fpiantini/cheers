57dd4b3e86219f36576036cc0f0fb8fb09d8dc36
Francesco Piantini <francesco.piantini@gmail.com>
2021-09-19 19:59:31 +0200

    improved BitBoard structure adding new() and PartialEq implementation and making internal state private

--- src/bin/itests.rs
+++ src/bin/itests.rs
@@ -1,6 +1,6 @@
 use cheers::bitboard::BitBoard;
 
 fn main() {
-    let bb = BitBoard { state: 0};
-    println!("Hello Bitboard. State = {}", bb.state);
+    let bb = BitBoard::new();
+    println!("Hello Bitboard. State = {:?}", bb);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,28 @@
 pub mod bitboard {
+
+    pub const EMPTY_STATE: u64 = 0;
+    pub static EMPTY_BITBOARD: BitBoard = BitBoard {state: EMPTY_STATE};
+
+    #[derive(Default, Debug)]
     pub struct BitBoard {
-        pub state: u64,
+        state: u64,
+    }
+    impl BitBoard {
+        pub fn new() -> BitBoard {
+            BitBoard {state: EMPTY_STATE}
+        }
+    }
+
+    impl PartialEq for BitBoard {
+        fn eq(&self, other: &BitBoard) -> bool {
+            self.state == other.state
+        }
     }
 
     #[test]
-    fn declare_empty_bitboard() {
-        let bb = BitBoard {state: 0};
-        assert_eq!(bb.state, 0);
+    fn by_default_a_new_bitboard_is_empty() {
+        let bb = BitBoard::new();
+        assert_eq!(bb.state, EMPTY_STATE);
+        assert_eq!(bb == EMPTY_BITBOARD, true);
     }
 }
