923739470e720a51d84dc4ac509fb96f121ef37f
Francesco Piantini <francesco.piantini@gmail.com>
2021-09-19 20:32:45 +0200

    added Cell enum

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,17 @@ pub mod bitboard {
     {
         Rank1, Rank2, Rank3, Rank4, Rank5, Rank6, Rank7, Rank8,
     }
+    enum Cell
+    {
+        A1, B1, C1, D1, E1, F1, G1, H1,
+        A2, B2, C2, D2, E2, F2, G2, H2,
+        A3, B3, C3, D3, E3, F3, G3, H3,
+        A4, B4, C4, D4, E4, F4, G4, H4,
+        A5, B5, C5, D5, E5, F5, G5, H5,
+        A6, B6, C6, D6, E6, F6, G6, H6,
+        A7, B7, C7, D7, E7, F7, G7, H7,
+        A8, B8, C8, D8, E8, F8, G8, H8,
+    }
 
     #[derive(Default, Debug, PartialEq, Eq)]
     pub struct BitBoard {
@@ -24,7 +35,8 @@ pub mod bitboard {
 
 
     // TESTS ---------------------------------------------------------
-    // Tests for File and Rank are quite obvious and can be removed in the future...
+    // Tests for File, Rank and Cell are quite obvious and
+    // can be removed in the future...
     #[test]
     fn file_a_has_index_0() {
         assert_eq!(File::FileA as i32, 0);
@@ -89,6 +101,14 @@ pub mod bitboard {
     #[test]
     fn rank_8_has_index_7() {
         assert_eq!(Rank::Rank8 as i32, 7);
+    }
+    #[test]
+    fn cell_a1_has_index_0() {
+        assert_eq!(Cell::A1 as i32, 0);
+    }
+    #[test]
+    fn cell_h8_has_index_63() {
+        assert_eq!(Cell::H8 as i32, 63);
     }
 
     #[test]
